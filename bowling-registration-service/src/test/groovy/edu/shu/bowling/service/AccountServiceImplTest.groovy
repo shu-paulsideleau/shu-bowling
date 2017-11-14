c7ee9a043c6658943043725f33cbcec4b088cdbe
robantonucci <rob.antonucci@me.com>
2017-11-14 11:05:11 -0500

    added unit tests to registration service

--- bowling-registration-service/src/test/groovy/edu/shu/bowling/service/AccountServiceImplTest.groovy
+++ bowling-registration-service/src/test/groovy/edu/shu/bowling/service/AccountServiceImplTest.groovy
@@ -1,6 +1,8 @@
 package edu.shu.bowling.service
 
 import edu.shu.bowling.common.AccountAlreadyExistException
+import edu.shu.bowling.common.PasswordComplexityException
+import edu.shu.bowling.common.UserIDException
 import edu.shu.bowling.model.Account
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -66,6 +68,46 @@ class AccountServiceImplTest extends Specification {
         final AccountAlreadyExistException exception = thrown()
         final message = exception.message
         message == "Account Already Exist"
+    }
+
+    def "Bad username"() {
+
+        given: "an account"
+        def account4 = new Account()
+        account4.setUserId("r&#)1!8")
+        account4.setFirstName("Jignesh")
+        account4.setLastName("Togadiya")
+        account4.setBirthDate(new Date())
+        account4.setEmail("rob@aol.com")
+        account4.setPhone("2035554442")
+        account4.setPassword("@df#Asder123")
+        when: "account is registered"
+        def result4 = accountService.register(account4)
+        then: "should throw exception"
+        final UserIDException exception = thrown()
+        final message = exception.message
+        message == "User name contains illegal characters."
+
+    }
+
+    def "Weak password"() {
+
+        given: "an account"
+        def account5 = new Account()
+        account5.setUserId("rob123")
+        account5.setFirstName("rob")
+        account5.setLastName("antonucci")
+        account5.setBirthDate(new Date())
+        account5.setEmail("rob2@aol.com")
+        account5.setPhone("2035551442")
+        account5.setPassword("password1")
+        when: "account is registered"
+        def result5 = accountService.register(account5)
+        then: "should throw exception"
+        final PasswordComplexityException exception = thrown()
+        final message = exception.message
+        message == "Password is not complex enough."
+
     }
 
 }
